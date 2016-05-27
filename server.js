05cd77662a810a903a66c3abb10a18b7ca3a0d76
Ivan <ivan.matellan@snoopconsulting.com>
2016-05-27 11:26:49 -0300

    2 designs

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var http = require('http').Server(app)
 var bodyParser = require('body-parser')
 var nodemailer = require('nodemailer')
 
+var os = require('os')
+var ifaces = os.networkInterfaces()
+
+
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
 app.use(express.static(__dirname+"/public"))
@@ -48,9 +52,31 @@ app.get('/index2', function(req, res){
 	res.sendFile(__dirname+"/index2.html")
 })
 
+app.get('/ip', function(req, res){
+	Object.keys(ifaces).forEach(function (ifname) {
+	  var alias = 0
+	  ifaces[ifname].forEach(function (iface) {
+	    if ('IPv4' !== iface.family || iface.internal !== false) {
+	      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+	      return
+	    }
+	    if (alias >= 1) {
+	      // this single interface has multiple ipv4 addresses
+	      console.log(ifname + ':' + alias, iface.address)
+	    } else {
+	      // this interface has only one ipv4 adress
+	      console.log(ifname, iface.address)
+	      res.json({'ip':iface.address})
+	    }
+	    ++alias
+	  })
+	})
+})
+
 var server = http.listen(8080, function(){
 	
 	var port = server.address().port
 	console.log("Server Running in http://127.0.0.1:"+port)
 	console.log("Base dir: "+__dirname)
 })
+
